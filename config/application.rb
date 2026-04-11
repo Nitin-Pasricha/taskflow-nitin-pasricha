9596511427420ed2f529d535f5ef9fc525f6fc4a
Nitin Pasricha <nitin@maropost.com>
2026-04-12 02:13:04 +0530

    Enabled PostgreSQL pgcrypto for UUID primary keys

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,10 @@ module TaskflowNitinPasricha
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # PostgreSQL UUID primary keys (TaskFlow spec); applies to `rails generate model`.
+    config.generators do |g|
+      g.orm :active_record, primary_key_type: :uuid
+    end
   end
 end
